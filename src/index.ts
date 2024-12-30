99324f7f44ea78326f3d7d2158f0ebe617f1522b
Leandro Lopes <leandro-lopes46@hotmail.com>
2024-12-30 13:30:46 -0300

    add primitive types example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hello World');
+const nome: string = 'Leandro'; // Qualquer tipo de string: '' "" ``
+const idade: number = 10; // 10, 1.57, -5.55
+const adulto: boolean = true; // true ou false
+const simbolo: symbol = Symbol('symbol-id');
+const big: bigint = 10n;
+const nulo: null = null;
+const indefinido: undefined = undefined;
