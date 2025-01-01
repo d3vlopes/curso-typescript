aa72afd2badf0395af047a7f5d96bdb041abef88
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-01-01 19:42:52 -0300

    add literal objects example with error

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const nome: string = 'Leandro'; // Qualquer tipo de string: '' "" ``
-const idade: number = 10; // 10, 1.57, -5.55
-const adulto: boolean = true; // true ou false
-const simbolo: symbol = Symbol('symbol-id');
-const big: bigint = 10n;
-const nulo: null = null;
-const indefinido: undefined = undefined;
+const objeto = {
+  chaveA: 'value',
+  chaveB: 'value',
+};
+
+// não podemos adicionar novas propriedades
+objeto.chaveC = 'value';
