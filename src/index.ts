dd97e8bd4fa9bcafd0b3a271aa705a3d0095754e
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-01-01 20:29:23 -0300

    add array constructor example

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-const arrayDeNumeros: number[] = [1, 2, 3];
-const arrayDeString: string[] = ['Josh', 'Patrick', 'Lamar'];
-const arrayDeStringENumeros: (string | number)[] = [1, 'Alice', 55];
+const arrayDeNumeros: Array<number> = [1, 2, 3];
+const arrayDeString: Array<string> = ['Josh', 'Patrick', 'Lamar'];
+const arrayDeStringENumeros: Array<string | number> = [1, 'Alice', 55];
 
 console.log(arrayDeString);
 console.log(arrayDeNumeros);
