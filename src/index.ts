3054f2a407ac88e39be7e0f00e0ee3a7ef65177f
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-01-15 23:46:10 -0300

    add type example

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,7 @@ interface ButtonAttributes {
   onClick: () => void;
 }
 
-interface ButtonProps extends ButtonAttributes {
-  variant: 'primary' | 'secondary' | 'outline';
-  size: 'small' | 'medium' | 'large';
-  isFullWidth?: boolean;
-}
+// ButtonProps tem as mesmas props que ButtonAttributes
+type ButtonProps = ButtonAttributes;
 
-// ButtonProps contem as props de ButtonAttributes mais as props dela
-const Button = ({
-  className,
-  onClick,
-  size,
-  variant,
-  isFullWidth = false,
-}: ButtonProps) => {};
+const Button = ({ className, onClick }: ButtonProps) => {};
