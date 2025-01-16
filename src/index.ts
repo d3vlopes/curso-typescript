04dcd203a86ef69b6d9c2413e8594c3e43bf12ca
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-01-15 23:22:37 -0300

    add methods interface example

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,12 @@ interface User {
   name: string;
   age: number;
   isAdmin: boolean;
+  getName: () => string;
 }
 
-// precisa conter as propriedades com os tipos definidos na interface User
 const customer: User = {
   name: 'Leandro',
   age: 30,
   isAdmin: false,
-};
-
-const admin: User = {
-  name: 'John Doe',
-  age: 34,
-  isAdmin: 'true', // não contém as propriedades com o mesmo tipo da interface User
+  getName: () => customer.name,
 };
