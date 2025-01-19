5ec60eaf5ce0e8f32ce49972538fab024077028f
Leandro Lopes <leandro-lopes46@hotmail.com>
2025-01-19 15:12:13 -0300

    add generic example

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-interface Identifier {
-  id: string;
-}
-interface Address {
-  street: string;
+function withGeneric<T>(value: T) {
+  return value;
 }
 
-interface User {
-  name: string;
-}
+// a função aceita o tipo que a gente passar entre <>
+const value1 = withGeneric<Date>(new Date());
+const value2 = withGeneric<string>('Leandro');
+const value3 = withGeneric<boolean>(true);
+const value4 = withGeneric<number>(15);
 
-type Customer = Address & User & Identifier;
-
-const customer: Customer = {
-  id: crypto.randomUUID(),
-  name: 'John Doe',
-  street: 'Loren ipsum dolor',
-};
+console.log(value1);
+console.log(value2);
+console.log(value3);
+console.log(value4);
